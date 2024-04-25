6ba2931303a63b32dc0071f45b93e754ffc7cc1f
CompilerCaster <job.kuzmingleb@gmail.com>
2024-04-25 14:52:49 +0800

    добавили весь проект

---
+++ boundary_changes.sql
@@ -0,0 +1,20 @@
+WITH RankedEvents AS (
+    SELECT
+        e.personnel_number,
+        b.employee_id,
+        TO_CHAR(b.set_at, 'YYYY-MM-DD HH24MISS') AS formatted_set_at,
+        ROW_NUMBER() OVER (PARTITION BY b.employee_id ORDER BY b.set_at DESC) AS rn
+    FROM
+        medrec.boundaries b
+        INNER JOIN structures.employees e ON e.id = b.employee_id
+    WHERE
+        e.org_id in (1464, 1465, 1466, 1467, 1468, 1469, 1470, 1471, 1472, 1473, 1474, 1475)
+        AND b.set_at BETWEEN '2023-08-01 000000.000' AND '2024-01-31 235959.999'
+)
+SELECT
+    personnel_number as Табельный номер,
+    formatted_set_at as Время последнего изменения гранни
+FROM
+    RankedEvents
+WHERE
+    rn = 1;
